0e58e7e71c9eb5c88a5e337afe0abca36ff98479
Sabree Blackmon <sabree@lovewellgroup.com>
2018-07-14 18:32:47 -0500

    Start app with Jaeger tracing enabled

--- backend/start_back.sh
+++ backend/start_back.sh
@@ -2,4 +2,4 @@
 
 export JAEGER_AGENT_HOST=`getent hosts jaeger | awk '{ print $1 }'`
 envsubst </etc/jaeger.yaml >/etc/envoy-jaeger.yaml
-donutbin --service_hostport="front-envoy:80" & 
+donutbin --service_hostport="front-envoy:80" --tracer_type="jaeger"
