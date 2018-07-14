a69c140e9e485722c27574e61b4e170159538858
Sabree Blackmon <sabree@lovewellgroup.com>
2018-07-14 15:25:36 -0500

    Additional Jaeger changes

--- backend/start_back.sh
+++ backend/start_back.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+envsubst </etc/jaeger.yaml >/etc/envoy-jaeger.yaml
 donutbin --service_hostport="front-envoy:80" & 
--- frontend/start_envoy.sh
+++ frontend/start_envoy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+envsubst </etc/jaeger.yaml >/etc/envoy-jaeger.yaml
 envoy -c /etc/envoy.json --service-cluster front-proxy --service-node `hostname`
