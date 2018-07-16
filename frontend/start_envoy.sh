e054765a9d8548800457b19216468594ad379ff4
Sabree Blackmon <sabree@lovewellgroup.com>
2018-07-16 00:23:50 -0500

    Fix of hot restart issues
    EPOCH switch required for proper script functioning

--- frontend/start_envoy.sh
+++ frontend/start_envoy.sh
@@ -2,4 +2,4 @@
 
 export JAEGER_AGENT_HOST=`getent hosts jaeger | awk '{ print $1 }'` 
 envsubst </etc/jaeger.yaml >/etc/envoy-jaeger.yaml
-envoy -c /etc/envoy.json --service-cluster front-proxy --service-node `hostname`
+envoy -c /etc/envoy.json --service-cluster front-proxy --service-node `hostname` --restart-epoch $RESTART_EPOCH
